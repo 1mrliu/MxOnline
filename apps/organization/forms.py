f1dcf71deb2bad53262a55c23e3949cc7edccc48
1mrliu <liudong@liudongdeMacBook-Pro.local>
2018-05-08 20:54:50 +0800

    增加前端课程modelform的功能实现

--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'liudong'
 __date__ = '2018/5/8 下午5:18'
+
+import re
 from django import forms
 from operation.models import UserAsk
 
@@ -15,3 +17,18 @@ class UserAskForm(forms.ModelForm):
     class Meta:
         model = UserAsk
         fields = ['name', 'mobile', 'course_name']
+
+    # 自定义的验证方式
+    def clean_mobile(self):
+        """
+        验证手机号码是否合法 
+        """
+        mobile = self.cleaned_data['mobile']
+        # re正则表达式
+        REGEX_MOBILE = "^1[358]\d{9}$|^147\d{8}$|^176\d{8}$"
+        p = re.compile(REGEX_MOBILE)
+        if p.match(mobile):
+            return mobile
+        else:
+            # 抛出异常
+            raise forms.ValidationError(u"手机号码非法", code='mobile_invilable')
