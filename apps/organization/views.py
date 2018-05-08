c1ee48a1d3deeaaf2f5855738611c270171e1a93
1mrliu <liudong@liudongdeMacBook-Pro.local>
2018-05-08 16:18:15 +0800

    使用开源库django-pure-pagination实现org-list界面的上下页跳转

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import View
 from .models import CourseOrg, CityDict
+
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
@@ -17,8 +19,19 @@ class OrgView(View):
         org_nums = all_orgs.count()
         # 城市
         all_citys = CityDict.objects.all()
+
+        # 对课程机构进行分页
+        try:
+            page = request.GET.get ('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        # 必须设置每页显示的数量 per_page的值
+        p = Paginator(all_orgs, 3,request=request)
+
+        orgs = p.page (page)
+
         return render(request, "org-list.html", {
-            "all_orgs": all_orgs,
+            "all_orgs": orgs,
             "all_citys": all_citys,
             "org_nums": org_nums,
         })
